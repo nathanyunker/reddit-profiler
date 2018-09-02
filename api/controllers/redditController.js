89a6db8a75cffd03eee89e86c4006de449c12dbb
Nathan Yunker <nathanyunker@rocketmail.com>
2018-09-01 20:52:02 -0700

    added some optimization

--- api/controllers/redditController.js
+++ api/controllers/redditController.js
@@ -15,21 +15,20 @@ exports.get_saved_posts = function(req, res) {
     const titles = [];
     let statusCode = 200;
     let lastId = '';
-    let uri = `user/${process.env.REDDIT_USER}/saved?limit=100`;
+    let baseUri = `user/${process.env.REDDIT_USER}/saved?limit=100`;
 
     function getSavedPosts(startingPoint) {
-        uri = startingPoint ? uri + '&after=t3_' + startingPoint : uri;
+        let uri = startingPoint ? baseUri + '&after=t3_' + startingPoint : baseUri;
     
         r._get({uri})
             .then(results => {
                 _.forEach(results, result => {
                     titles.push(result.title);
-                    lastId = result.id;
                 })
 
                 if (results.length == 100) {
-                    console.log('Polling another page with this id', lastId);
-                    getSavedPosts(lastId);
+                    console.log('Polling another page with this id', _.last(results).id);
+                    getSavedPosts(_.last(results).id);
                 } else {
                     res.json(statusCode, {"titles": titles});
                 }
